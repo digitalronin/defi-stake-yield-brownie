138f0bb8c2a7f04995d3ddfac1934a56cd07bcb2
digitalronin <david@digitalronin.com>
2022-03-10 13:02:36 +0900

    WiP: TokenFarm contract

---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// stakeTokens
+// unStakeTokens
+// issueTokens
+// addAllowedTokens
+// getEthValue
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenFarm is Ownable {
+  address[] public allowedTokens;
+
+  function stakeTokens(uint256 _amount, address _token) public {
+    require(_amount > 0, "Amount must be more than zero.");
+  }
+
+  function addAllowedTokens(address _token) public onlyOwner {
+    allowedTokens.push(_token);
+  }
+
+  function tokenIsAllowed(address _token) public view returns (bool) {
+    for(uint256 i=0; i < allowedTokens.length; i++) {
+      if (allowedTokens[i] == _token) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
