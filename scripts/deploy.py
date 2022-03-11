924cdcc6423369d6beae5fe0a55e6d7e66b3b303
digitalronin <david@digitalronin.com>
2022-03-11 15:12:32 +0900

    Add a deployment script and mocks for local dev.

---
+++ scripts/deploy.py
@@ -0,0 +1,52 @@
+from scripts.utils import get_account, get_contract
+from brownie import TokenFarm, DappToken, config, network
+from web3 import Web3
+
+RETAINED_BALANCE = Web3.toWei(1_000, "ether")
+
+
+def main():
+    account = get_account()
+    tf, dapp_token = deploy_token_farm_and_dapp_token(account)
+    fund_token_farm(tf, dapp_token, account)
+    add_allowed_tokens(tf, account, dapp_token)
+    return (tf, dapp_token)
+
+
+def deploy_token_farm_and_dapp_token(account):
+    ntwrk = config["networks"][network.show_active()]
+
+    dapp = DappToken.deploy(
+        {"from": account},
+        publish_source=ntwrk.get("verify", False)
+    )
+
+    tf = TokenFarm.deploy(
+        dapp.address,
+        {"from": account},
+        publish_source=ntwrk.get("verify", False)
+    )
+
+    return (tf, dapp)
+
+
+def fund_token_farm(farm, token, account):
+    token.transfer(
+        farm.address,
+        token.totalSupply() - RETAINED_BALANCE,
+        {"from": account},
+    ).wait(1)
+
+
+def add_allowed_tokens(farm, account, dapp_token):
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    allowed_tokens = {
+        dapp_token: get_contract("dai_usd_price_feed"),
+        fau_token: get_contract("dai_usd_price_feed"),
+        weth_token: get_contract("eth_usd_price_feed"),
+    }
+    for token in allowed_tokens:
+        farm.addAllowedTokens(token.address, {"from": account}).wait(1)
+        pf = allowed_tokens[token]
+        farm.setPriceFeedAddress(token.address, pf.address, {"from": account})
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,10 +2,21 @@ from brownie import (
     network,
     config,
     accounts,
+    Contract,
+    MockDAI,
+    MockWETH,
+    MockV3Aggregator,
 )
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "ganache-ui"]
+
+contract_to_mock = {
+    "weth_token": MockDAI,
+    "fau_token": MockWETH,
+    "dai_usd_price_feed": MockV3Aggregator,
+    "eth_usd_price_feed": MockV3Aggregator,
+}
 
 
 def get_account(index=None, id=None):
@@ -24,3 +35,42 @@ def get_account(index=None, id=None):
         account = accounts.add(config["wallets"]["from_key"])
 
     return account
+
+
+def get_contract(contract_name):
+    """
+    Grab contract addresses from brownie-config if defined, or deploy a mock
+    version of that contract and return it.
+
+        Args:
+            contract_name (string)
+
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently
+            deployed version of this contract.
+
+    """
+    contract_type = contract_to_mock[contract_name]
+    net_name = network.show_active()
+    ntwrk = config["networks"][net_name]
+
+    if net_name in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = ntwrk[contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
+
+
+def deploy_mocks():
+    account = get_account()
+    weth = MockWETH.deploy({"from": account})
+    print(f"Deployed mock WETH contract: {weth.address}")
+    fau = MockDAI.deploy({"from": account})
+    print(f"Deployed mock FAU contract: {fau.address}")
+    pf = MockV3Aggregator.deploy(8, 2000 * 10**8, {"from": account})
+    print(f"Deployed mock Price Feed contract: {pf.address}")
