ad376e038dd5edfee3ca58d33b2161ce7b9b1b84
digitalronin <david@digitalronin.com>
2022-03-12 11:56:59 +0900

    Add Connect/Disconnect button
    This prompts the user to connect their web3 wallet to the site, and
    responds to disconnection, using the useDapp library.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {DAppProvider, ChainId} from "@usedapp/core"
+import {Header} from "./components/Header"
 
 function App() {
   return (
     <DAppProvider config={{
       supportedChains: [ChainId.Rinkeby]
     }}>
+      <Header></Header>
       <div>Hi!</div>
     </DAppProvider>
   )
