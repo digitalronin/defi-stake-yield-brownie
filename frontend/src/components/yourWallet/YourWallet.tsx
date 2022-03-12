e8408872c31bfb65bc07f395ab09d7e74ab99bab
digitalronin <david@digitalronin.com>
2022-03-12 16:10:40 +0900

    WiP: Add tabs to the YourWallet component

--- frontend/src/components/yourWallet/YourWallet.tsx
+++ frontend/src/components/yourWallet/YourWallet.tsx
@@ -1,3 +1,7 @@
+import {useState} from "react"
+import {Box} from "@material-ui/core"
+import {TabContext, TabList, TabPanel} from "@material-ui/lab"
+import {Tab} from "@material-ui/core"
 import {Token} from "../Main"
 
 interface YourWalletProps {
@@ -5,5 +9,25 @@ interface YourWalletProps {
 }
 
 export const YourWallet = ({supportedTokens}: YourWalletProps) => {
-  return (<div>YourWallet</div>)
+  const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0)
+
+  return (
+    <Box>
+      <h1>Your Wallet</h1>
+      <Box>
+        <TabContext value={selectedTokenIndex.toString()}>
+          <TabList aria-label="stake form tabs">
+            {supportedTokens.map((token, index) => {
+              return (
+                <Tab label={token.name}
+                  value={index.toString()}
+                  key={index}
+                />
+              )
+            })}
+          </TabList>
+        </TabContext>
+      </Box>
+    </Box>
+  )
 }
