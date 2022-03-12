ab67df2223b60c2769287bdb164be3872d223b72
digitalronin <david@digitalronin.com>
2022-03-12 15:26:23 +0900

    Try to handle local dev network and 'real' ones
    The idea is that the front-end works whether it's
    pointing at a real blockchain (such as Rinkeby),
    or a local ganache-cli instance (with persistent
    build artefacts enabled).
    
    This is tricky, because useEthers() reports the
    chainId as 1337, but brownie uses "dev" in the
    map.json file instead.
    
    This code seems to work, but I get a weird issue
    when using Metamask, targeting localhost:8545 - 
    when I first try to connect to the application,
    metamask asks me to approve a contract deployment
    transaction from the deployer account. I *think*
    this is something to do with nonces, where
    metamask things there's an outstanding transaction
    that needs to be approved. This would make sense,
    because I'm resetting my local blockchain when I
    restart it, which would account for nonces going
    out of sync.
    
    It doesn't happen on Rinkeby, so I'm just going to
    live with it, for now.

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,10 +1,51 @@
 import {useEthers} from "@usedapp/core"
+import {constants} from "ethers"
 import chainNames from "../chain-names.json"
+import mapJson from "../chain-info/deployments/map.json"
+import brownieConfig from "../brownie-config.json"
 
 export const Main = () => {
   const {chainId} = useEthers()
-  const networkName = chainId ? chainNames[chainId] : "dev"
+  const networkName = chainId ? chainNames[String(chainId)] : "dev"
+
+  const {dappTokenAddress, wethTokenAddress, fauTokenAddress} = getTokenContractAddresses(chainId)
+
+  function getTokenContractAddresses(chainId: any) {
+    let rtn
+
+    if (chainId === undefined) {
+      rtn = {
+        dappTokenAddress: constants.AddressZero,
+        wethTokenAddress: constants.AddressZero,
+        fauTokenAddress: constants.AddressZero
+      }
+    } else if (chainId === 1337) {
+      // local ganache-cli deployment, so contract addresses are in map.json
+      const data = mapJson["dev"]
+      rtn = {
+        dappTokenAddress: data.DappToken,
+        wethTokenAddress: data.MockWETH,
+        fauTokenAddress: data.MockDAI
+      }
+    } else {
+      // 'real' deployment, so contract addresses are in brownie-config.json
+      // except DappToken, which is deployed, so comes from map.json
+      rtn = {
+        dappTokenAddress: mapJson[String(chainId)]["DappToken"][0],
+        wethTokenAddress: brownieConfig["networks"][networkName]["weth_token"],
+        fauTokenAddress: brownieConfig["networks"][networkName]["fau_token"]
+      }
+    }
+
+    return rtn
+  }
+
   return (
-    <div>{networkName}</div>
+    <div>
+      {networkName}<br />
+      {dappTokenAddress}<br />
+      {wethTokenAddress}<br />
+      {fauTokenAddress}<br />
+    </div>
   )
 }
