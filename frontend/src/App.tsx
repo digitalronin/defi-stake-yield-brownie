2227f5b40d3abdf15ed4bf0fae2ee88ec236f043
digitalronin <david@digitalronin.com>
2022-03-13 14:45:24 +0900

    Bugfix: Enable testing against local ganache-cli
    See https://github.com/TrueFiEng/useDApp/issues/440#issuecomment-1000645827
    
    To find the contract address, look at the 
    ganache-cli log and grab the address to which it
    deploys, when you click the connect button on the
    web app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,10 @@ import {Container} from "@material-ui/core"
 function App() {
   return (
     <DAppProvider config={{
-      networks: [Rinkeby]
+      networks: [Rinkeby],
+      multicallAddresses: {
+        "1337": "0x30375b532345b01cb8c2ad12541b09e9aa53a93d",
+      },
     }}>
       <Header></Header>
       <Container maxWidth="md">
