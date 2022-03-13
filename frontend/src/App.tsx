6a9845bc58a82d292b527b68e1683731fef13067
digitalronin <david@digitalronin.com>
2022-03-13 15:03:37 +0900

    Use notifications to report transaction progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,10 @@ function App() {
   return (
     <DAppProvider config={{
       networks: [Rinkeby],
+      notifications: {
+        expirationPeriod: 1000,
+        checkInterval: 1000
+      },
       multicallAddresses: {
         "1337": "0x30375b532345b01cb8c2ad12541b09e9aa53a93d",
       },
--- frontend/src/components/yourWallet/StakeForm.tsx
+++ frontend/src/components/yourWallet/StakeForm.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import {formatUnits} from "@ethersproject/units"
 import {utils} from "ethers"
-import {useEthers, useTokenBalance} from "@usedapp/core"
+import {useEthers, useTokenBalance, useNotifications} from "@usedapp/core"
 import {Button, Input} from "@material-ui/core"
 import {Token} from "../Main"
 import {useStakeTokens} from "../../hooks/useStakeTokens"
@@ -15,6 +15,7 @@ export const StakeForm = ({token}: StakeFormProps) => {
   const {account} = useEthers()
   const tokenBalance = useTokenBalance(tokenAddress, account)
   const formattedBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
+  const {notifications} = useNotifications()
 
   const [amount, setAmount] = useState<number | string | Array<number | string>>(0)
 
@@ -29,6 +30,21 @@ export const StakeForm = ({token}: StakeFormProps) => {
     const amountAsWei = utils.parseEther(amount.toString())
     return approveAndStake(amountAsWei.toString())
   }
+
+  useEffect(() => {
+    if (notifications.filter(
+      (notification) =>
+        notification.type === "transactionSucceed" &&
+        notification.transactionName === "Approve ERC20 transfer").length > 0) {
+      console.log("Approved")
+    }
+    if (notifications.filter(
+      (notification) =>
+        notification.type === "transactionSucceed" &&
+        notification.transactionName === "Stake tokens").length > 0) {
+      console.log("Staked")
+    }
+  }, [notifications])
 
   return (
     <div>
