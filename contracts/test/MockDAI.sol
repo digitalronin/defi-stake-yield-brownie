0cb9f58a275504337babf991170cd9d2b6f1fe3e
digitalronin <david@digitalronin.com>
2022-03-14 12:22:00 +0900

    Mint 1M MockDAI and MockWETH tokens
    This gives us some balances to play with, when
    using the front end with a local ganache-cli chain

--- contracts/test/MockDAI.sol
+++ contracts/test/MockDAI.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockDAI is ERC20 {
-  constructor() public ERC20("Mock DAI", "DAI") {}
+  constructor() public ERC20("Mock DAI", "DAI") {
+    _mint(msg.sender, 1000000 * 10**18);
+  }
 }
--- contracts/test/MockWETH.sol
+++ contracts/test/MockWETH.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockWETH is ERC20 {
-  constructor() public ERC20("Mock WETH", "WETH") {}
+  constructor() public ERC20("Mock WETH", "WETH") {
+    _mint(msg.sender, 1000000 * 10**18);
+  }
 }
